e9c1332702f7a717c1e6987d06512e86793b1316
YahiyaSTC <yahiyastc@gmail.com>
2022-12-12 08:03:05 -0800

    second-commit-12-12-22

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function () {
+    $(".all1").owlCarousel({
+      loop: true,
+      navigation: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: false,
+      items: 3,
+      mousewheel: false,
+      dots: true,
+      slideBy: 1,
+      
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true,
+        },
+        200: {
+          items: 1,
+          nav: false,
+        },
+        300: {
+          items: 1,
+          nav: false,
+        },
+        400: {
+          items: 1,
+          nav: false,
+        },
+        700: {
+          items: 1,
+          nav: false,
+        },
+        900: {
+          items: 3,
+          nav: false,
+        },
+        1000: {
+          items: 3,
+          nav: false,
+          loop: true,
+        },
+      },
+    });
+  });
